7471ea55e28577cbb4b2e33c2505176bf5d5efe5
Michael van Tellingen <michael@mvantellingen.nl>
2019-09-27 10:04:02 +0200

    Use markdown for the readme

---
+++ README.md
@@ -0,0 +1,50 @@
+<!-- start-no-pypi -->
+[![codecov](https://codecov.io/gh/labd/django-session-timeout/branch/master/graph/badge.svg)](https://codecov.io/gh/labd/django-session-timeout)
+[![pypi](https://img.shields.io/pypi/v/django-session-timeout.svg)](https://pypi.python.org/pypi/django-session-timeout/)
+[![readthedocs](https://readthedocs.org/projects/django-session-timeout/badge/)](https://django-session-timeout.readthedocs.io/en/latest/)
+[![tests](https://github.com/labd/django-session-timeout/workflows/Python%20Tests/badge.svg)](https://github.com/labd/django-session-timeout/actions)
+<!-- end-no-pypi -->
+
+# django-session-timeout
+
+Add timestamp to sessions to expire them independently
+
+## Installation
+
+```shell
+pip install django-session-timeout
+```
+
+## Usage
+
+Update your settings to add the SessionTimeoutMiddleware:
+
+```python
+MIDDLEWARE_CLASSES = [
+    # ...
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django_session_timeout.middleware.SessionTimeoutMiddleware',
+    # ...
+]
+```
+
+And also add the ``SESSION_EXPIRE_SECONDS``:
+
+
+```python
+SESSION_EXPIRE_SECONDS = 3600  # 1 hour
+```
+
+By default, the session will expire X seconds after the start of the session.
+To expire the session X seconds after the `last activity`, use the following setting:
+
+```python
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY = True
+```
+
+By default, `last activiy` will be grouped per second.
+To group by different period use the following setting:
+
+```python
+SESSION_EXPIRE_AFTER_LAST_ACTIVITY_GRACE_PERIOD = 60 # group by minute
+```
--- setup.py
+++ setup.py
@@ -2,65 +2,61 @@ import re
 
 from setuptools import find_packages, setup
 
-docs_require = [
-    'sphinx>=1.8.4',
-]
+docs_require = ["sphinx>=1.8.4"]
 
 tests_require = [
-    'coverage==4.5.3',
-    'freezegun==0.3.11',
-    'pytest==4.3.1',
-    'pytest-django==3.4.8',
-    'pytest-cov==2.6.1',
-
+    "coverage==4.5.3",
+    "freezegun==0.3.11",
+    "pytest==4.3.1",
+    "pytest-django==3.4.8",
+    "pytest-cov==2.6.1",
     # Linting
-    'isort==4.3.15',
-    'flake8==3.7.7',
-    'flake8-blind-except==0.1.1',
-    'flake8-debugger==3.1.0',
+    "isort==4.3.15",
+    "flake8==3.7.7",
+    "flake8-blind-except==0.1.1",
+    "flake8-debugger==3.1.0",
 ]
 
-with open('README.rst') as fh:
+with open("README.md") as fh:
     long_description = re.sub(
-        '^.. start-no-pypi.*^.. end-no-pypi', '', fh.read(), flags=re.M | re.S)
-
+        "<!-- start-no-pypi -->.*<!-- end-no-pypi -->\n",
+        "",
+        fh.read(),
+        flags=re.M | re.S,
+    )
 
 setup(
-    name='django-session-timeout',
-    version='0.0.4',
+    name="django-session-timeout",
+    version="0.0.4",
     description="Middleware to expire sessions after specific amount of time",
     long_description=long_description,
-    url='https://github.com/LabD/django-session-timeout',
+    long_description_content_type="text/markdown",
+    url="https://github.com/LabD/django-session-timeout",
     author="Lab Digital",
     author_email="opensource@labdigital.nl",
-    install_requires=[
-        'Django>=1.11',
-        'six>=1.12',
-    ],
+    install_requires=["Django>=1.11", "six>=1.12"],
     tests_require=tests_require,
-    extras_require={
-        'docs': docs_require,
-        'test': tests_require,
-    },
+    extras_require={"docs": docs_require, "test": tests_require},
     use_scm_version=True,
     entry_points={},
-    package_dir={'': 'src'},
-    packages=find_packages('src'),
+    package_dir={"": "src"},
+    packages=find_packages("src"),
     include_package_data=True,
-    license='MIT',
+    license="MIT",
     classifiers=[
-        'Development Status :: 5 - Production/Stable',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Framework :: Django :: 1.11',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
     ],
     zip_safe=False,
 )
