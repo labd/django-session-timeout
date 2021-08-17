f93abaff139bd868621dfcd463da2bc918a3309b
ewoudenberg-kasisto <eric.woudenberg@kasisto.com>
2021-08-17 15:15:57 -0400

    Add ability to ignore certain URL paths when determining whether there is activity.

--- src/django_session_timeout/middleware.py
+++ src/django_session_timeout/middleware.py
@@ -1,4 +1,5 @@
 import time
+import re
 
 from django.conf import settings
 from django.contrib.auth.views import redirect_to_login
@@ -14,6 +15,18 @@ SESSION_TIMEOUT_KEY = "_session_init_timestamp_"
 
 
 class SessionTimeoutMiddleware(MiddlewareMixin):
+    def __init__(self, get_response=None):
+        self.get_response = get_response
+        self.ignored_paths_regex = None
+
+        regex = getattr(settings, "SESSION_ACTIVITY_IGNORED_PATHS_REGEX", None)
+        if regex:
+            self.ignored_paths_regex = re.compile(regex)
+
+    def is_path_ignored(self, path):
+        ignored = self.ignored_paths_regex and bool(self.ignored_paths_regex.search(path))
+        return ignored
+
     def process_request(self, request):
         if not hasattr(request, "session") or request.session.is_empty():
             return
@@ -41,5 +54,7 @@ class SessionTimeoutMiddleware(MiddlewareMixin):
             settings, "SESSION_EXPIRE_AFTER_LAST_ACTIVITY_GRACE_PERIOD", 1
         )
 
-        if expire_since_last_activity and time.time() - init_time > grace_period:
+        if expire_since_last_activity \
+            and time.time() - init_time > grace_period \
+            and not self.is_path_ignored(request.path):
             request.session[SESSION_TIMEOUT_KEY] = time.time()
